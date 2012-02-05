4416b78743173e04b88706325f61bd6240676e1f
Paul Tagliamonte <tag@pault.ag>
2012-02-05 09:17:35 -0500

    adding in kruft

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# Copyright (c) Sunlight Labs, 2012 under the terms and conditions
+# of the LICENSE file.
+
+from sunlight import __appname__, __version__
+from setuptools import setup
+
+long_description = open('README.rst').read()
+
+setup(
+    name       = __appname__,
+    version    = __version__,
+    packages   = [ 'charlie' ],
+
+    author       = "Paul Tagliamonte",
+    author_email = "paultag@ubuntu.com",
+
+    long_description = long_description,
+    description      = 'Boston MBTA Data',
+    license          = "BSD",
+    url              = "https://github.com/paultag/python-charlie",
+
+    platforms        = ['any']
+)
