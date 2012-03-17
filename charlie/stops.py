bee15bcc26dfb18d61f9455897474b698a119006
Paul Tagliamonte <paultag@sunlightfoundation.com>
2012-03-17 15:55:18 -0400

    cosmetic changes.

--- charlie/stops.py
+++ charlie/stops.py
@@ -1,4 +1,4 @@
-# 
+#
 
 stops = {}
 
@@ -15,39 +15,47 @@ class Stop:
     def _goodie(self, name):
         return self.goodies[name]
 
-    ## K'plah!
-    # Line,PlatformKey,PlatformName,StationName,PlatformOrder,StartOfLine,
-    # EndOfLine,Branch,Direction,stop_id,stop_code,stop_name,stop_desc,stop_lat,
-    # stop_lon
-    
     def getLine(self):
         return self._goodie("Line")
+
     def getPlatformKey(self):
         return self._goodie("PlatformKey")
+
     def getPlatformName(self):
         return self._goodie("PlatformName")
+
     def getStationName(self):
         return self._goodie("StationName")
+
     def getPlatformOrder(self):
         return self._goodie("PlatformOrder")
+
     def isStartOfLine(self):
         return self._goodie("StartOfLine") == "TRUE"
+
     def isEndOfLine(self):
         return self._goodie("EndOfLine") == "TRUE"
+
     def getBranch(self):
         return self._goodie("Branch")
+
     def getDirection(self):
         return self._goodie("Direction")
+
     def getStopID(self):
         return self._goodie("stop_id")
+
     def getStopCode(self):
         return self._goodie("stop_code")
+
     def getStopName(self):
         return self._goodie("stop_name")
+
     def getStopDescription(self):
         return self._goodie("stop_desc")
+
     def getLat(self):
         return self._goodie("stop_lat")
+
     def getLon(self):
         return self._goodie("stop_lon")
-
