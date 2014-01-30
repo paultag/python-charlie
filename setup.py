d48906af98f40268c2d7c2df6e4cc5b601905ef4
Paul Tagliamonte <tag@pault.ag>
2014-01-29 19:20:07 -0500

    start to play with some ideas

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
-# Copyright (c) Sunlight Labs, 2012 under the terms and conditions
-# of the LICENSE file.
+# 
 
 from charlie import __appname__, __version__
 from setuptools import setup
@@ -10,8 +9,7 @@ long_description = open('README.rst').read()
 setup(
     name       = __appname__,
     version    = __version__,
-    packages   = [ 'charlie',
-                   'platform_data' ],
+    packages   = [ 'charlie',],
 
     author       = "Paul Tagliamonte",
     author_email = "paultag@ubuntu.com",
